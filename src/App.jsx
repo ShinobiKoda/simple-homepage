21b1997ed1f4d5a0fdf288ac7067db9adf6b0ad1
shinobiKoda <sirp2804@gmail.com>
2025-02-06 20:00:15 +0100

    implement sidebar and animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <></>
+    <>
+      <Navbar />
+      <HeroSection />
+    
+    </>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import Favicon from "../assets/images/favicon.ico";
+import Hamburger from "../assets/images/hamburger-button.svg";
+import CloseIcon from "../assets/images/close-button.svg";
+import Moon from "../assets/images/Moon_fill_light.svg";
+import Sun from "../assets/images/Sun_fill.svg";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <nav className="w-full px-4 flex items-center justify-between">
+      <div className="flex gap-1 items-center">
+        <img src={Favicon} alt="favicon" className="w-12" />
+        <p className="font-bold text-2xl">Alarado</p>
+      </div>
+      <div>
+        <img src={Hamburger} alt="hamburger" onClick={()=> setIsOpen(!isOpen)}/>
+
+        <div className={`sidebar fixed top-0 right-0 flex flex-col gap-3 h-full bg-black text-white shadow-lg transform ${isOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-300 ease-in-out w-64 z-20`}>
+          <img src={CloseIcon} alt="close-button" onClick={()=> setIsOpen(false)} className="w-12 fixed right-0 top-0"/>
+          <div className="flex flex-col gap-3 mt-12 p-3">
+          <div className="bg-black rounded-2xl flex items-center justify-between w-[5rem] py-[0.2rem] px-1">
+            <img src={Moon} alt="Moon-icon"/>
+            <div className="w-6 h-6 rounded-[50%] bg-white flex items-center justify-center">
+              <img src={Sun} alt="" />
+            </div>
+          </div>
+          <ul className="nav-links">
+            <li>
+              <a href="#" className="hover:opacity-90 cursor-pointer" onClick={() => setIsOpen(false)}>About us</a>
+            </li>
+            <li>
+              <a href="#" className="hover:opacity-90 cursor-pointer" onClick={() => setIsOpen(false)}>Product</a>
+            </li>
+            <li>
+              <a href="#" className="hover:opacity-90 cursor-pointer" onClick={() => setIsOpen(false)}>Resource</a>
+            </li>
+            <li>
+              <a href="#" className="hover:opacity-90 cursor-pointer" onClick={() => setIsOpen(false)}>Contact</a>
+            </li>
+          </ul>
+          </div>
+        </div>
+      </div>
+      {isOpen && (
+        <div className="inset-0 fixed bg-black bg-opacity-50 z-10" onClick={()=> setIsOpen(false)}></div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
